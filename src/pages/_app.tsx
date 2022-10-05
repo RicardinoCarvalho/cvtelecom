883485b41ad501f1b40e755455fb8cb1c54c1be6
RicardinoCarvalho <ricardino.carvalho477@gmail.com>
2022-10-05 12:13:26 -0100

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import {AppProps} from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import {ReactQueryDevtools} from 'react-query/devtools'
+import {theme} from '../styles/theme'
+
+import { QueryClientProvider } from 'react-query'
+import { SidebarDrawerProvider } from '../contexts/SidebarDawerContext'
+import { makeServer } from '../servises/mirage'
+import { queryClient } from '../servises/queryClient'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
+
+
+function MyApp({ Component, pageProps }): AppProps {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+
+}
+
+export default MyApp
